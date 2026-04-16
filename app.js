36cc43c0900e95c7b9d23d8064a5deb763e2e7e3
Sam11dot <samruddhichaudhari185@gmail.com>
2026-04-16 15:13:36 +0530

    updated design

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 
 // ✅ Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://dummy.shreemai.com",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
